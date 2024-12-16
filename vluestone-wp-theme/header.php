81302f854c1fd00785b21026e5d85254e6f420b4
Erik Sensitivo <erik.contato15@gmail.com>
2024-12-16 13:35:11 -0300

    Ajustando imagem em destaque

--- vluestone-wp-theme/header.php
+++ vluestone-wp-theme/header.php
@@ -104,7 +104,9 @@
         <div id="header-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img class="w-100" src="img/carousel-1.jpg" alt="Image">
+                    <?php $slide1 = get_field('carrossel');
+                    ?>
+                    <img class="w-100" src="<?php echo $slide1 ?>" alt="Image">
                     <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                         <div class="p-3" style="max-width: 900px;">
                             <h5 class="text-white text-uppercase mb-3 animated slideInDown">Solução digital </h5>
