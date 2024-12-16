c27e6bff7cc61f2da00665b1b824454b34a9e8ff
Erik Sensitivo <erik.contato15@gmail.com>
2024-12-16 12:38:02 -0300

    Iniciando tema Wordpress

---
+++ vluestone-wp-theme/functions.php
@@ -0,0 +1,19 @@
+<?php
+// Enqueue theme styles and scripts
+function vluestone_enqueue_assets() {
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
+    wp_enqueue_style('theme-style', get_template_directory_uri() . '/css/style.css');
+	wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.js', array( 'jquery' ), null, true );
+}
+
+add_action('wp_enqueue_scripts', 'vluestone_enqueue_assets');
+
+// Register theme menus
+function vluestone_register_menus() {
+    register_nav_menus(array(
+        'primary' => __('Primary Menu', 'vluestone'),
+        'footer'  => __('Footer Menu', 'vluestone'),
+    ));
+}
+add_action('after_setup_theme', 'vluestone_register_menus');
+?>
